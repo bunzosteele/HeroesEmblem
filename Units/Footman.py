e1aaf49b45fee97f10ba81175504c243cc32173b
bunzosteele <aaronsteele@comcast.net>
2014-10-25 01:42:14 -0700

    Make footman transparent

--- Units/Footman.py
+++ Units/Footman.py
@@ -3,6 +3,10 @@ from pygame.locals import *
 from Units.Unit import *
 
 class Footman(Unit):
+	Image = "images/Footman.png"
+	MaxHealth = 10
+	AttackPower = 10
+	Defense = 5
    
-    def __init__(self, x , y, movement):
-		Unit.__init__(self, "images/Knight.jpg", x, y, movement, 10, 5)
+	def __init__(self, x , y, movement):
+		Unit.__init__(self, Footman.Image, x, y, movement, Footman.MaxHealth, Footman.Defense)
