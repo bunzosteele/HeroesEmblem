f34f23260e059a38289beee471202511dea1d68f
123than <epvoon@gmail.com>
2014-10-25 10:46:01 -0700

    Mage updated.

--- Units/Mage.py
+++ Units/Mage.py
@@ -1,4 +1,12 @@
 import sys, pygame, os
 from pygame.locals import *
+from Units.Unit import *
 
-class
+class Mage(Unit):
+	Image = "images/mage.jpg"
+	MaxHealth = 8
+	AttackPower = 13
+	Defense = 3
+   
+	def __init__(self, x , y, movement):
+		Unit.__init__(self, Mage.Image, x, y, movement, Mage.MaxHealth, Mage.Defense)
