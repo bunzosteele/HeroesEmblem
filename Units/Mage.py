77495485b1d787b3f6f7132a396dcd6d4caf56af
bunzosteele <aaronsteele@comcast.net>
2014-10-25 13:22:04 -0700

    nothing really

--- Units/Mage.py
+++ Units/Mage.py
@@ -1,12 +1,11 @@
-import sys, pygame, os
-from pygame.locals import *
 from Units.Unit import *
 
+
 class Mage(Unit):
-	Image = "images/mage.jpg"
-	MaxHealth = 8
-	AttackPower = 13
-	Defense = 3
-   
-	def __init__(self, x , y, movement):
-		Unit.__init__(self, Mage.Image, x, y, movement, Mage.MaxHealth, Mage.Defense)
+    Image = "images/mage.jpg"
+    MaxHealth = 8
+    AttackPower = 13
+    Defense = 3
+
+    def __init__(self, x, y, movement):
+        Unit.__init__(self, Mage.Image, x, y, movement, Mage.MaxHealth, Mage.Defense)
