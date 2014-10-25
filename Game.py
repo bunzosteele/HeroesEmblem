9ebc6c96220d1ec9fd91b497878b40e71370e172
duncanbart <drbart.duncan@gmail.com>
2014-10-25 12:11:23 -0700

    Added movement shadow

--- Game.py
+++ Game.py
@@ -20,27 +20,52 @@ def handle_movement(units, which_unit):
         which_unit = change_unit(unit_size, which_unit)
     return which_unit
 
+def click_movement(units, which_unit, clicked_space, selected):
+    unit_x = units[which_unit].x/Tile.Size
+    unit_y = units[which_unit].y/Tile.Size
+    click_x = clicked_space[0]
+    click_y = clicked_space[1]
+    total_distance = abs((unit_x - click_x)) + abs((unit_y - click_y))
+    if total_distance <= units[which_unit].movement:
+        units[which_unit].x = click_x*Tile.Size
+        units[which_unit].y = click_y*Tile.Size
+        selected = False
+    return selected
+
+def draw_shadow(x, y, selected_color, tile_size, movement, screen):
+    temp_x = x * tile_size
+    temp_y = y * tile_size
+    battlefield.tiles[y][x].draw(screen, x, y)
+    pygame.gfxdraw.box(screen, pygame.Rect(temp_x, temp_y, Tile.Size, Tile.Size), selected_color)
+    if movement != 0:
+        draw_shadow(x + 1, y, selected_color, tile_size, movement - 1, screen)
+        draw_shadow(x - 1, y, selected_color, tile_size, movement - 1, screen)
+        draw_shadow(x, y + 1, selected_color, tile_size, movement - 1, screen)
+        draw_shadow(x, y - 1, selected_color, tile_size, movement - 1, screen)
+
+
 pygame.init()
 running = True
 
 battlefield = Battlefield([
-	[Mountain(), Mountain(), Mountain(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
-	[Mountain(), Mountain(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
-	[Mountain(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
-	[Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
-	[Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
-	[Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
-	[Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Mountain()],
-	[Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Mountain(), Mountain()],
-	[Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Mountain(), Mountain(), Mountain()]])
+    [Mountain(), Mountain(), Mountain(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
+    [Mountain(), Mountain(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
+    [Mountain(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
+    [Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
+    [Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
+    [Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass()],
+    [Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Mountain()],
+    [Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Mountain(), Mountain()],
+    [Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Grass(), Mountain(), Mountain(), Mountain()]])
 
 pixelWidth = len(battlefield.tiles[0]) * Tile.Size
 pixelHeight = len(battlefield.tiles) * Tile.Size
 screen_size = width, height = pixelWidth, pixelHeight
 screen = pygame.display.set_mode(screen_size)
 battlefield.draw(screen)
+selected_color = (100, 115, 245, 100)
 
-unit1 = Footman(0 , 0, 6)
+unit1 = Footman(0, 0, 4)
 
 clock = pygame.time.Clock()
 
@@ -57,15 +82,7 @@ while running:
             pos = pygame.mouse.get_pos()
             clicked_space = (pos[0]/Tile.Size, pos[1]/Tile.Size)
             if selected:
-                unit_x = units[which_unit].x/Tile.Size
-                unit_y = units[which_unit].y/Tile.Size
-                click_x = clicked_space[0]
-                click_y = clicked_space[1]
-                total_distance = abs((unit_x - click_x)) + abs((unit_y - click_y))
-                if total_distance <= units[which_unit].movement:
-                    units[which_unit].x = click_x*Tile.Size
-                    units[which_unit].y = click_y*Tile.Size
-                    selected = False
+                selected = click_movement(units, which_unit, clicked_space, selected)
             else:
                 for i in range(0, unit_size):
                     if clicked_space == units[i].get_location():
@@ -84,20 +101,14 @@ while running:
             if event.key == pygame.K_RIGHT: 
                 units[which_unit].move_right()
                 which_unit = handle_movement(units, which_unit)
-				
+
     battlefield.draw(screen)
-    unit1.draw(screen)
-    #unit2.draw(screen)
-    #unit3.draw(screen)
 
     if selected:
         location = units[which_unit].get_location()
-        x,y = location[0], location[1]
-        x = x * Tile.Size
-        y = y * Tile.Size
-        pygame.gfxdraw.box(screen, pygame.Rect(x,y,Tile.Size,Tile.Size), (100,115,245,100))
-        #pygame.draw.rect(screen, light_grey,(x,y,space_size,space_size),0)
-    
+        x, y = location[0], location[1]
+        draw_shadow(x, y, selected_color, Tile.Size, units[which_unit].movement, screen)
+    unit1.draw(screen)
     pygame.display.update()
 
     clock.tick(60)
